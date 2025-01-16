45e2e5217688657fccc7faa9c77420b8acf3eaaf
Agustinus Kristiadi <agustinus@kristia.de>
2025-01-15 19:37:26 -0500

    Fix URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import icon from "astro-icon";
 export default defineConfig({
   // site: "https://approximateinference.org",
   site: "https://approximateinference.github.io",
+  base: "approximateinference",
   prefetch: true,
   integrations: [
     expressiveCode(expressiveCodeOptions),
